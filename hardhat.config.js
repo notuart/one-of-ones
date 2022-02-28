0727354ea3c889eba414eaae33c6a4faf7784de7
no+u <benoit.villiere@icloud.com>
2022-02-28 21:52:30 +0000

    Add deployment script

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,8 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("hardhat-gas-reporter");
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+// npx hardhat run scripts/deploy.js --network rinkeby
+
 module.exports = {
   solidity: {
     version: "0.8.4",
@@ -19,5 +18,17 @@ module.exports = {
     enabled: false,
     currency: "USD",
     gasPrice: 100,
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    mainnet: {
+      url: process.env.MAINNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
 };
