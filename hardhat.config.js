a80bb121c142f82b78748a70dd515b212b222579
no+u <benoit.villiere@icloud.com>
2022-02-28 21:58:31 +0000

    Deploy test contract on Rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 require("hardhat-gas-reporter");
 
 // npx hardhat run scripts/deploy.js --network rinkeby
+// Contract deployed to: 0xF4c038360C1931b357996ddA45C0E69B49A520eD
+// npx hardhat verify --network rinkeby 0xF4c038360C1931b357996ddA45C0E69B49A520eD
 
 module.exports = {
   solidity: {
@@ -30,5 +34,8 @@ module.exports = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
